a71c2185f682fcb15c47d87084e9b8c2b624530f
Adriel <adriel.fsantos@outlook.com>
2023-09-30 12:19:14 -0300

    Configura pacote flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/screens/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MainApp());
 }
 
