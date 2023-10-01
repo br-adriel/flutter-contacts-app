1b45ca837e21b14458df41a0a63561778c7a1250
Adriel <adriel.fsantos@outlook.com>
2023-10-01 19:15:09 -0300

    Implementa salvamento de contato

--- lib/screens/form_contato.dart
+++ lib/screens/form_contato.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/models/contato.dart';
+import 'package:flutter_contacts/repositories/back4app/contatos.dart';
 import 'package:flutter_contacts/utils/input_generator.dart';
 import 'package:flutter_contacts/widgets/imagem_perfil_input.dart';
 import 'package:image_picker/image_picker.dart';
@@ -15,20 +17,44 @@ class FormContatoScreen extends StatefulWidget {
 class _FormContatoScreenState extends State<FormContatoScreen> {
   XFile? _imagem;
 
-  final TextEditingController _nome = TextEditingController(text: "");
-  final TextEditingController _sobrenome = TextEditingController(text: "");
-  final TextEditingController _telefonePadrao = TextEditingController(text: "");
-  final TextEditingController _emailPadrao = TextEditingController(text: "");
-  final InputGenerator _telefones = InputGenerator(
+  final _contatosB4ARepository = ContatosB4ARepository();
+
+  final _nome = TextEditingController(text: "");
+  final _sobrenome = TextEditingController(text: "");
+  final _telefonePadrao = TextEditingController(text: "");
+  final _emailPadrao = TextEditingController(text: "");
+  final _telefones = InputGenerator(
     countStart: 2,
     inputType: TextInputType.phone,
     label: "Telefone",
   );
-  final InputGenerator _emails = InputGenerator(
+  final _emails = InputGenerator(
     countStart: 2,
     inputType: TextInputType.emailAddress,
     label: "Email",
   );
+
+  bool _loading = false;
+
+  _salvar() async {
+    List<String> emails = [_emailPadrao.text, ..._emails.controllersValues];
+    List<String> telefones = [
+      _telefonePadrao.text,
+      ..._telefones.controllersValues
+    ];
+    var contato = ContatoModel(
+      nome: _nome.text,
+      emails: emails,
+      imagem: _imagem == null ? "" : _imagem!.path,
+      sobrenome: _sobrenome.text,
+      telefones: telefones,
+    );
+    _loading = true;
+    setState(() {});
+    await _contatosB4ARepository.adicionar(contato);
+    _loading = false;
+    setState(() {});
+  }
 
   _adicionarCampoTelefone() {
     _telefones.adicionarCampo();
@@ -50,66 +76,71 @@ class _FormContatoScreenState extends State<FormContatoScreen> {
       appBar: AppBar(
         title: Text("${widget.atualizar ? "Atualizar" : "Adicionar"} contato"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: "Salvar",
-        child: const Icon(Icons.save),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          ImagemDePerfilInput(_imagem),
-          TextField(
-            decoration: const InputDecoration(labelText: "Nome"),
-            textInputAction: TextInputAction.next,
-            controller: _nome,
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: "Sobrenome"),
-            textInputAction: TextInputAction.next,
-            controller: _sobrenome,
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                child: TextField(
-                  decoration: const InputDecoration(labelText: "Telefone 1"),
+      floatingActionButton: _loading
+          ? null
+          : FloatingActionButton(
+              onPressed: _salvar,
+              tooltip: "Salvar",
+              child: const Icon(Icons.save),
+            ),
+      body: _loading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView(
+              padding: const EdgeInsets.all(16),
+              children: [
+                ImagemDePerfilInput(_imagem),
+                TextField(
+                  decoration: const InputDecoration(labelText: "Nome"),
                   textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.phone,
-                  controller: _telefonePadrao,
+                  controller: _nome,
                 ),
-              ),
-              IconButton(
-                onPressed: _adicionarCampoTelefone,
-                icon: const Icon(Icons.add, color: Colors.blue),
-                tooltip: "Adicionar outro",
-              )
-            ],
-          ),
-          ..._telefones.gerarCampos(afterRemove: _aposRemoverCampo),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                child: TextField(
-                  decoration: const InputDecoration(labelText: "Email 1"),
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: _emailPadrao,
+                TextField(
+                  decoration: const InputDecoration(labelText: "Sobrenome"),
+                  textInputAction: TextInputAction.next,
+                  controller: _sobrenome,
                 ),
-              ),
-              IconButton(
-                onPressed: _adicionarCampoEmail,
-                icon: const Icon(Icons.add, color: Colors.blue),
-                tooltip: "Adicionar outro",
-              )
-            ],
-          ),
-          ..._emails.gerarCampos(afterRemove: _aposRemoverCampo),
-          const SizedBox(height: 64)
-        ],
-      ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        decoration:
+                            const InputDecoration(labelText: "Telefone 1"),
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.phone,
+                        controller: _telefonePadrao,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: _adicionarCampoTelefone,
+                      icon: const Icon(Icons.add, color: Colors.blue),
+                      tooltip: "Adicionar outro",
+                    )
+                  ],
+                ),
+                ..._telefones.gerarCampos(afterRemove: _aposRemoverCampo),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        decoration: const InputDecoration(labelText: "Email 1"),
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _emailPadrao,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: _adicionarCampoEmail,
+                      icon: const Icon(Icons.add, color: Colors.blue),
+                      tooltip: "Adicionar outro",
+                    )
+                  ],
+                ),
+                ..._emails.gerarCampos(afterRemove: _aposRemoverCampo),
+                const SizedBox(height: 64)
+              ],
+            ),
     );
   }
 }
