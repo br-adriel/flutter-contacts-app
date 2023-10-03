453357f91adbca88dd8935d657351099eaef2e85
Adriel <adriel.fsantos@outlook.com>
2023-10-02 23:25:28 -0300

    Implementa exibição de imagem de contato

--- lib/widgets/contato_list_tile.dart
+++ lib/widgets/contato_list_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/models/contato.dart';
 
@@ -11,7 +13,11 @@ class ContatoListTile extends StatelessWidget {
     return InkWell(
       onTap: () {},
       child: ListTile(
-        leading: const CircleAvatar(),
+        leading: CircleAvatar(
+          foregroundImage: _contato.imagem.isEmpty
+              ? const AssetImage("assets/img/profile.jpg")
+              : FileImage(File(_contato.imagem)) as ImageProvider,
+        ),
         title: Text("${_contato.nome} ${_contato.sobrenome}"),
         trailing: IconButton(
           onPressed: () {},
